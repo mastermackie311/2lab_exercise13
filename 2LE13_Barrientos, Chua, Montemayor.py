757e7b12c3ab2f54f84184d77ff0d75eb1ff4648
montemayormarkjefferson <montemayor.markjefferson@gmail.com>
2026-05-01 13:15:02 +0800

    appending

--- 2LE13_Barrientos, Chua, Montemayor.py
+++ 2LE13_Barrientos, Chua, Montemayor.py
@@ -8,3 +8,17 @@ while True:
         print("File already exists")
     except FileNotFoundError:
         print("File not found")
+    
+    def addItem():
+        with open("shopee.txt", "a") as file:
+            itemName = input("Item name: ")
+            itemQuantity = int(input("Item quantity: "))
+            itemPrice = float(input("Item price: "))
+            file.write(f"Item: {itemName}, Quantity: {itemQuantity}, Price: {itemPrice}\n")
+            print("\n", itemName, "has been added to your cart.")
+   
+    def viewCart():
+        with open("shopee.txt", "r") as file:
+            content = file.read()
+            print("\n--- Shopping Cart ---")
+            print(content)
